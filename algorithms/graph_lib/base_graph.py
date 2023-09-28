2fa151e4047d12f2bdbb2017b50c0c609129438a
edwinyu2005 <edwinyu2005@gmail.com>
2023-09-27 23:29:33 -0700

    Add graph-related classes

---
+++ algorithms/graph_lib/base_graph.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+from typing import List, Optional, Union
+
+
+class BaseNode:
+    """
+    A base class for nodes in a graph.
+
+    Attributes:
+    - id (Union[int, str]): A unique identifier for the node.
+    """
+    def __init__(self, id: Union[int, str]):
+        self.id = id
+
+    def __str__(self) -> str:
+        return str(self.id)
+
+
+class BaseEdge:
+    """
+    A base class for edges in a graph.
+
+    Attributes:
+    - id (Union[int, str]): A unique identifier for the edge.
+    - weight (float): Weight/cost associated with traversing the edge.
+    """
+    def __init__(self, id: Union[int, str], weight: float = 1.0) -> None:
+        self.id = id
+        self.weight = weight
+
+    def __str__(self) -> str:
+        return "Edge(ID: {}, Weight: {})".format(self.id, self.weight)
+
+
+class BaseGraph:
+    """
+    Base class for representing a graph.
+
+    Attributes:
+    - nodes (Dict[Union[int, str], BaseNode]): A dictionary mapping node IDs to node instances.
+    - edges (Dict[Union[int, str], BaseEdge]): A dictionary mapping edge IDs to edge instances.
+    """
+    def __init__(self) -> None:
+        self.nodes = {}
+        self.edges = {}
+
+    def add_node(self, node: BaseNode) -> None:
+        if node.id in self.nodes:
+            raise ValueError("A node with ID {} already exists.".format(node.id))
+        self.nodes[node.id] = node
+
+    def remove_node(self, node_id: Union[int, str]) -> None:
+        if node_id in self.nodes:
+            # TODO: implement
+            pass
+
+    def add_edge(self, edge: BaseEdge) -> None:
+        """Add an edge to the graph."""
+        if edge.id in self.edges:
+            raise ValueError("An edge with ID {} already exists.".format(edge.id))
+        self.edges[edge.id] = edge
+
+    def remove_edge(self, edge_id: Union[int, str]) -> None:
+        if edge_id in self.edges:
+            # TODO: implement
+            pass
+
+    def get_node(self, node_id: Union[int, str]) -> Optional[BaseNode]:
+        return self.nodes.get(node_id)
+
+    def get_edge(self, edge_id: Union[int, str]) -> Optional[BaseEdge]:
+        return self.edges.get(edge_id)
+
+    def get_neighbors(self, node_id: Union[int, str]) -> List[BaseNode]:
+        """Abstract method to be implemented by subclasses to get neighbors of a node."""
+        raise NotImplementedError
