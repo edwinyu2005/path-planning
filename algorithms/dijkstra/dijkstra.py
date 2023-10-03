e8798ca32722743b6160420cc4130a298c9cc093
edwinyu2005 <edwinyu2005@gmail.com>
2023-10-02 23:12:47 -0700

    Add rendering method for Dijkstra

--- algorithms/dijkstra/dijkstra.py
+++ algorithms/dijkstra/dijkstra.py
@@ -1,7 +1,10 @@
+import networkx as nx
+import matplotlib.pyplot as plt
 import heapq
 from typing import Dict, Optional, Tuple, Union
 
 from algorithms.graph_lib.base_graph import BaseGraph
+from algorithms.graph_lib.directed_graph import DirectedGraph
 
 
 class Dijkstra:
@@ -51,3 +54,49 @@ class Dijkstra:
         }
 
         return shortest_paths
+
+    def render(self, source_node_id: Union[int, str], target_node_id: Union[int, str]) -> None:
+        """
+        Visualize the graph and the shortest path computed by Dijkstra's algorithm.
+
+        Parameters:
+        - source_node_id: Union[int, str]
+            The ID of the source node from which the shortest paths are computed.
+        - target_node_id: Union[int, str]
+            The ID of the target node to which the shortest path will be highlighted and displayed.
+
+        Note:
+        This method uses `networkx` and `matplotlib` to create a visual representation of
+        the graph. The nodes and edges are drawn using `networkx` and `matplotlib`, and the
+        computed shortest path from source_node_id to target_node_id is highlighted in red.
+        """
+        if isinstance(self.graph, DirectedGraph):
+            G = nx.DiGraph()
+        else:
+            G = nx.Graph()
+
+        # Convert graph to a networkx graph
+        for node_id in self.graph.nodes.keys():
+            G.add_node(node_id)
+            for neighbor in self.graph.get_neighbors(node_id):
+                edge = self.graph.get_edge_between(node_id, neighbor.id)
+                G.add_edge(node_id, neighbor.id, weight=edge.weight)
+
+        # Run the algorithm
+        shortest_paths = self.find_shortest_paths(source_node_id)
+        tree_edges = [
+            (predecessor, node_id)
+            for node_id, (_, predecessor) in shortest_paths.items()
+            if predecessor is not None
+        ]
+
+        # Visualize
+        pos = nx.spring_layout(G)
+        nx.draw(G, pos, with_labels=True, node_size=700, node_color='skyblue', font_weight='bold', arrowsize=20)
+        labels = nx.get_edge_attributes(G, 'weight')
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+        nx.draw_networkx_edges(G, pos, edgelist=tree_edges, edge_color='r', width=2, arrowsize=20)
+
+        # Display
+        plt.title('Shortest Path from {source_node_id} to {shortest_path[-1]}: {" -> ".join(shortest_path)}')
+        plt.show()
